874eb82024e681a6369b9eb4861c0fefa23ab172
Yuwailaifeng <yuwailaifeng@gmail.com>
2017-07-24 23:36:37 +0800

    .h   .cpp

--- HelloCPP.cpp
+++ HelloCPP.cpp
@@ -1,19 +1,8 @@
 /*域外来风*/
-#include<iostream>
-using namespace std;
-
-#define NEWLINE '\n'
-const double  PI = 3.14159265358979323846;
-static int count = 10; /* 全局变量 */
-
-					   // 函数声明
-void swap(int *x, int *y);
+#include "HelloCPP.h"
 
 
-
-
-
-int main()
+int main(int argc, char* argv[])
 {
 
 #if 0
